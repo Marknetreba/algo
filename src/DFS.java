e7568acb83c0b9ca9162c88b8754301a2c7f2517
mnetreba <mark.netreba@griddynamics.com>
2019-06-03 18:52:22 +0400

    Update

--- src/DFS.java
+++ src/DFS.java
@@ -1,10 +1,27 @@
 import java.util.Iterator;
 
-public class DFS {
+import java.io.*;
+import java.util.*;
 
-    // V - vertices of graph
-    // adj - Adjacency Lists of graph
+class GraphDFS {
+    private int V;   // No. of vertices
 
+    // Array  of lists for Adjacency List Representation
+    private LinkedList<Integer> adj[];
+
+    GraphDFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    //Function to add an edge into the graph
+    void addEdge(int v, int w) {
+        adj[v].add(w);  // Add w to v's list.
+    }
+
+    // A function used by DFS
     void DFSUtil(int v, boolean visited[]) {
         // Mark the current node as visited and print it
         visited[v] = true;
@@ -27,5 +44,21 @@ public class DFS {
 
         // Call the recursive helper function to print DFS traversal
         DFSUtil(v, visited);
+    }
+
+    public static void main(String args[]) {
+        GraphDFS g = new GraphDFS(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Depth First Traversal " +
+                "(starting from vertex 2)");
+
+        g.DFS(2);
     }
 }
